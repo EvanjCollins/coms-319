bdbe198f82981a05c06dd01adfca3bb01c2415a5
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-02-01 21:05:03 -0600

    Lab 2 Commit

---
+++ Lab 2/validation1.js
@@ -0,0 +1,16 @@
+function validateForm() {
+  var firstName = document.forms["userInput"]["firstName"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+  var lastName = document.forms["userInput"]["lastName"].value;
+
+
+  var gender = document.forms["userInput"]["gender"].value;
+
+
+  var state = document.forms["userInput"]["state"].value;
+
+
+}
