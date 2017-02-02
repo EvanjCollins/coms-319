2ba4d4f09a27c9f5483bca4cf7018c3c1733b322
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-02-01 22:05:11 -0600

    New Commit

--- Lab 2/validation1.js
+++ Lab 2/validation1.js
@@ -1,16 +1,43 @@
 function validateForm() {
-  var firstName = document.forms["userInput"]["firstName"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
+
+  var firstName = document.forms["userInput"].elements["firstName"].value;
+  var result = validateText(firstName);
+  if(result == true){
+    //place checkmark there
+  }
+  else{
+    //place big x
+  }
+
+  var lastName = document.forms["userInput"].elements["lastName"].value;
+  result = validateText(lastName);
+  if(result == true){
+    //place checkmark there
+  }
+  else{
+    //place big x
+  }
+
+//  var gender = document.forms["userInput"].elements["gender"].value;
+    // must be entered
+
+//  var state = document.forms["userInput"].elements["state"].value;
+    // must be entered
+    // save to local storage?
+
+    function validateText(textInput){
+      var bool = true;
+      for(var i=0; i<textInput.length; i++){
+        var value = textInput.charCodeAt(i);
+        if(value < 48 || value > 122){
+          alert("WRONG");
+          bool = false;
+        }
+        else{
+          alert("TRUE");
+        }
+      }
+      return bool;
     }
-  var lastName = document.forms["userInput"]["lastName"].value;
-
-
-  var gender = document.forms["userInput"]["gender"].value;
-
-
-  var state = document.forms["userInput"]["state"].value;
-
 
 }
