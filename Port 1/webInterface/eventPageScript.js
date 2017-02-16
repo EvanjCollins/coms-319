691616c4a041f77e00fd8221f390fac207af9595
Matthew Vanderwerf <matthewwerf@gmail.com>
2017-02-16 16:26:25 -0600

    Working PUT Request

--- Port 1/webInterface/eventPageScript.js
+++ Port 1/webInterface/eventPageScript.js
@@ -20,14 +20,14 @@ function getUserEvents(){
 
 	for(var i=0; i< responseData.message.length; i++){
 		appendString += "<tr><td>";
-		appendString += genEventRow(responseData.message[i]);
+		appendString += genEventRow(responseData.message[i], i);
 		appendString += "</td></tr>";
-		console.log(responseData.message[i]);
+		//console.log(responseData.message[i]);
 	}
 	$('#usersEventTable').append(appendString);
 }
 
-function genEventRow(eventJSON){
+function genEventRow(eventJSON, num){
 	var returnString = "";
 	returnString += "Event Name: " + eventJSON.eventName + "<br>";
 	returnString += "Event Description: " + eventJSON.eventDescription + "<br>";
@@ -45,8 +45,71 @@ function genEventRow(eventJSON){
 			returnString += "<br>";
 		}
 	}
+	returnString += "<input onclick=\"rsvp(";
+	returnString += num;
+	returnString += ")\" value=\"RSVP to " + eventJSON.eventName + "\" type=\"button\" id=\"Event" + num + "\"";
 
 	return returnString;
+}
+
+function getIndex(arr, val) {
+    console.log(arr);
+    for (var i=0; i<arr.length; i++) {
+        if (arr[i].Name == val){
+		return i;
+	}
+    }
+    return -1;
+}
+
+function rsvp(eventNum){
+	var eID = "Event" + eventNum;
+	var eventElement = document.getElementById(eID);
+	var eventName = eventElement.value;
+	var eventName = eventName.split(" ");
+	var eventQuery = "";
+
+	for(var i=2; i<eventName.length; i++){
+		eventQuery += eventName[i];
+		eventQuery += " ";
+	}
+
+	//location.reload();
+	var url = "http://proj-319-104.cs.iastate.edu:3000/events/byEventName/" + eventQuery;
+	var responseData;
+	$.ajax({
+                url: url,
+                dataType: 'json',
+                async: false,
+                success: function(data) {
+                        //console.log(data);
+                	responseData = data;
+                }
+        });
+	var user = localStorage.Name;
+	var index = getIndex(responseData.message[0].invitedUsers, user);
+	responseData.message[0].invitedUsers[index].RSVP = true;
+	console.log(responseData.message[0]);
+	responseData.message[0].invitedUsers = JSON.stringify(responseData.message[0].invitedUsers);
+	//var formData = {"eventName": responseData.message[0].eventName, "eventDescription": responseData.message[0].eventDescription, "invitedUsers": responseData.message[0].invitedUsers};
+        //var formData = {"invitedUsers": test[0]};
+	try{
+                $.ajax({
+                        url: "http://proj-319-104.cs.iastate.edu:3000/events/byEventName/" + eventQuery,
+                        dataType: 'json',
+                        type: "PUT",
+                        data: responseData.message[0],
+                        async: false,
+                        success: function(data) {
+                               // console.log(data);
+                        }
+                });
+        }
+        catch(err){
+                console.log(err);
+        }
+        location.reload();
+
 }
 
 function eventGetRequest(){
@@ -59,9 +122,18 @@ function createNewEvent(){
   var eventDescription = document.getElementById("eventDescription").value;
   var invitedFriends = document.getElementById("invitedFriends").value;
   //need to parse friend list and turn into json object for form data
+  var friendArray = invitedFriends.split(",");
+  var friendsJSON = "[";
+  for(var i=0; i<friendArray.length-1; i++){
+        friendArray[i] = friendArray[i].trim();
+  	friendsJSON += "{\"Name\": \"" + friendArray[i] + "\",\"RSVP\": false},";
+  }
+  friendArray[i] = friendArray[i].trim();
+  friendsJSON += "{\"Name\": \"" + friendArray[i] + "\",\"RSVP\": false}";
+  friendsJSON += "]";
+  //console.log(friendsJSON);
 
-
-  var formData = {"eventName": eventName, "eventDescription": eventDescription, "invitedUsers": invitedFriends};
+  var formData = {"eventName": eventName, "eventDescription": eventDescription, "invitedUsers": friendsJSON};
   	try{
         	$.ajax({
                 	url: "http://proj-319-104.cs.iastate.edu:3000/events/",
@@ -77,4 +149,5 @@ function createNewEvent(){
         catch(err){
         	console.log(err);
         }
+	location.reload();
 }
